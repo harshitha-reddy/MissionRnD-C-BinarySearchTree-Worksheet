5ac45cb56bf15b324c767b4c22481565eaa35ff2
harshitha-reddy <harshitha-reddy@missionrnd.com>
2016-03-30 22:39:11 +0530

    Submit Commit

--- src/SortedArraytoBST.cpp
+++ src/SortedArraytoBST.cpp
@@ -27,6 +27,7 @@ Note : Return Null for invalid Inputs
 Note : Donot create a new BST .
 */
 #include <stdlib.h>
+struct node* array_to_bst_recr(int *arr, int first, int last);
 struct node{
 	struct node * left;
 	int data;
@@ -35,7 +36,19 @@ struct node{
 
 
 struct node * convert_array_to_bst(int *arr, int len){
-	
+	if (arr==NULL)
 	return NULL;
+	int i = 0, j = len - 1;
+	return array_to_bst_recr(arr, i, j);
 }
 
+struct node* array_to_bst_recr(int *arr, int first, int last){
+	if (first > last)
+		return NULL;
+	int mid = (first + last) / 2;
+	struct  node *root = (struct node*)malloc(sizeof(struct node));
+	root->data = arr[mid];
+	root->left = array_to_bst_recr(arr, first, mid - 1);
+	root->right = array_to_bst_recr(arr, mid + 1, last);
+	return root;
+}
